31c3a616ad9c96552edb3b9d1dc69fbe554628dc
Pablo Chico de Guzman <pchico83@gmail.com>
2019-10-03 16:54:21 -0700

    Rename es to com

--- src/main/java/com/okteto/helloworld/Application.java
+++ src/main/java/com/okteto/helloworld/Application.java
@@ -1,4 +1,4 @@
-package es.okteto.helloworld;
+package com.okteto.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/okteto/helloworld/RestHelloWorld.java
+++ src/main/java/com/okteto/helloworld/RestHelloWorld.java
@@ -1,4 +1,4 @@
-package es.okteto.helloworld;
+package com.okteto.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
